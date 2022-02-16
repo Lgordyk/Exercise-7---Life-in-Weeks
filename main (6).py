326a1b447f6c7e5176a7cb80648a0ab2907c34f7
Lgordyk <99572889+Lgordyk@users.noreply.github.com>
2022-02-15 20:47:56 -0500

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+LifeLeft = 90-int(age)
+
+WeeksofLife = LifeLeft*52
+MonthsofLife = LifeLeft*12
+DaysofLife = LifeLeft*365
+
+print("You have " + str(DaysofLife) + " days, " + str(WeeksofLife) + " weeks, and " + str(MonthsofLife) + " months left.")
+
+
+
